07893618121ace8c6f3160cf03188a75deda00f7
ionlizarazu <ilizarazu@codesyntax.com>
2022-12-20 13:21:01 +0100

    babel config

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,1 @@
-module.exports = function (api) {
-  api.cache(true);
-  const presets = ['razzle/babel'];
-  const plugins = [
-    [
-      'react-intl', // React Intl extractor, required for the whole i18n infrastructure to work
-      {
-        messagesDir: './build/messages/',
-      },
-    ],
-  ];
-
-  return {
-    plugins,
-    presets,
-  };
-};
+module.exports = require('@plone/volto/babel');
